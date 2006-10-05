402692c34fd5ef6d4567ae695b51e0776a947155
saulius <devnull@localhost>
2006-10-05 15:28:28 +0300

    [svn] Moved remotely

---
+++ ControllerNotFoundException.php
@@ -0,0 +1,38 @@
+<?php
+
+ClassLoader::import("framework.ApplicationException");
+
+/**
+ * Thrown when controller specified in request does not exists.
+ * 
+ * @package	framework
+ * @author Saulius Rupainis <saulius@integry.net>
+ */
+class ControllerNotFoundException extends ApplicationException {
+
+	/**
+	 * Name of controller that was not found 
+	 */
+	private $controllerName;
+
+
+	/**
+	 * @param string $controller Controllers name
+	 */
+	public function __construct($controllerName) {
+		parent::__construct("Specified controller ($controllerName) does not exist");
+		$this->controllerName = $controllerName;
+	}
+
+	/**
+	 * Gets a controller name
+	 *
+	 * @return string Controller name
+	 */
+	public function getControllerName() {
+		return $this->controllerName;
+	}
+
+}
+
+?>
