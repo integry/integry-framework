d72737273dd64b980864add664b87d4f61e7ba6c
saulius <devnull@localhost>
2006-10-05 15:28:26 +0300

    [svn] Moved remotely

---
+++ ClassLoaderException.php
@@ -0,0 +1,14 @@
+<?php
+
+ClassLoader::import("framework.ApplicationException");
+
+/**
+ * Indicates that class loader failed loading some source file
+ *
+ * @package framework
+ * @author Saulius Rupainis <saulius@integry.net>
+ */
+class ClassLoaderException extends ApplicationException {
+}
+
+?>
