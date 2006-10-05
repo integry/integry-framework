0c8872fb4c0899790a993b21f8a0c2da8e2af333
saulius <devnull@localhost>
2006-10-05 15:28:12 +0300

    [svn] Moved remotely

---
+++ ApplicationException.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * General exception which might be raised within an application context
+ *
+ * @package framework
+ * @author Saulius Rupainis <saulius@integry.net>
+ */
+class ApplicationException extends Exception {
+}
+
+?>
