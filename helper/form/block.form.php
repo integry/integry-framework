af71833c97c85cb0fe4eb91ae5d58b18f86abdcc
saulius <devnull@localhost>
2006-11-03 16:08:26 +0200

    [svn] ""

--- helper/form/block.form.php
+++ helper/form/block.form.php
@@ -62,7 +62,7 @@ function smarty_block_form($params, $content, $smarty, &$repeat)
 		require_once("function.includeJs.php");
 		smarty_function_includeJs(array("file" => "library/formvalidator.js"), $smarty);
 		
-		$validatorField = '<input type="hidden" name="_validator" value="' . $handle->getValidator()->getJSValidatorParams() . '"/>';
+		$validatorField = '<input type="hidden" class="hidden" name="_validator" value="' . $handle->getValidator()->getJSValidatorParams() . '"/>';
 	}
 	else
 	{
