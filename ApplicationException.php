94697e1411fb06be224151518277de37fc37e8e3
sergej <devnull@localhost>
2006-11-06 11:48:21 +0200

    [svn] ""

--- ApplicationException.php
+++ ApplicationException.php
@@ -15,10 +15,10 @@ class ApplicationException extends Exception
 		$i = 0;
 		foreach($this->getTrace() as $call)
 		{
-			if(isset($call['file']) && !in_array($call['file'], $showedFiles))
+			if(isset($call['file']) && isset($call['line']) && !isset($showedFiles[$call['file']][$call['line']]))
 			{
-				$showedFiles[] = $call['file'];
-				echo ($i++).': '.$call['file'].'<br />';
+				$showedFiles[$call['file']][$call['line']] = true;
+				echo '<strong>'.($i++).'</strong>: '.$call['file'].': '.$call['line'].'<br />';
 			}
 		}
 	}
