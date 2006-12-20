4f64c5d4a618af5becdcae87e9b8cc33e89f8a9b
saulius <devnull@localhost>
2006-12-20 15:00:16 +0200

    [svn]

--- ClassLoader.php
+++ ClassLoader.php
@@ -212,6 +212,9 @@ class ClassLoader
 	 */
 	public static function importPath($path, $check = true)
 	{
+		if (substr($path, -1, 1) == DIRECTORY_SEPARATOR) {
+			$path = substr($path, 0, strlen($path) - 1);
+		}
 		$oldPath = get_include_path();
 		if ($check)
 		{
