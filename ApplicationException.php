825f06f73112888cb6b835e2c840bb60b7178718
sergej <devnull@localhost>
2007-01-22 15:00:49 +0200

    [svn] Show more user friendly call trace when Exception is thrown

--- ApplicationException.php
+++ ApplicationException.php
@@ -1,4 +1,8 @@
 <?php
+class Test
+{
+    
+}
 
 /**
  * General exception which might be raised within an application context
@@ -9,18 +13,80 @@
 class ApplicationException extends Exception 
 {
 			
-	public function getFileTrace()
+	public static function getFileTrace($trace)
 	{
 		$showedFiles = array();
 		$i = 0;
-		foreach($this->getTrace() as $call)
+		$traceString = '';
+				
+		$ajax = isset($_SERVER['HTTP_X_REQUESTED_WITH']) ? true : false;
+		
+		foreach($trace as $call)
 		{
 			if(isset($call['file']) && isset($call['line']) && !isset($showedFiles[$call['file']][$call['line']]))
 			{
 				$showedFiles[$call['file']][$call['line']] = true;
-				echo '<strong>'.($i++).'</strong>: '.$call['file'].': '.$call['line'].'<br />';
+				
+				// Get file name and line
+				if($ajax) 
+				{
+				    $position = ($i++).": {$call['file']}:{$call['line']}";
+				}
+				else 
+				{
+				    $position = "<strong>".($i++)."</strong>: \"{$call['file']}\":{$call['line']}";
+				}
+				
+				// Get function name
+				if(isset($call['class']) && isset($call['type']) && isset($call['function']))
+				{
+				    $functionName = "{$call['class']}{$call['type']}{$call['function']}";
+				} 
+				else 
+				{
+				    $functionName = $call['function'];
+				}
+				
+				// Get function arguments
+				$arguments = '';
+				$j = 1;
+				foreach($call['args'] as $argv) 
+				{
+				    switch(gettype($argv)) 
+				    {
+				        case 'string':
+				            $arguments .= "\"$argv\"";
+			            break;
+				        case 'boolean':
+				             $arguments .= ($argv ? 'true' : 'false');
+			            break;
+				        case 'integer':
+				        case 'double':
+				             $arguments .= $argv;
+			            break;
+				        case 'object':
+				             $arguments .= "(object)" . get_class($argv);
+				        break;
+				        case 'array':
+				             $arguments .= "Array";
+			            break;
+				        default:
+				            $arguments .= $argv;
+			            break;
+				    }
+				    
+				    if($j < count($call['args'])) $arguments .= ", "; $j++;
+				}
+				
+				// Get new line
+				$newLine = $ajax ? "\n" : "<br />\n";
+				
+				// format the output line
+				$traceString .= "$position - $functionName($arguments) $newLine";
 			}
 		}
+		
+		return $traceString;
 	}
 }
 
