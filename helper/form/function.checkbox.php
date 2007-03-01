25e7a300622794db3deb12240dffebb196986d11
sergej <devnull@localhost>
2007-03-01 13:12:28 +0200

    [svn]

--- helper/form/function.checkbox.php
+++ helper/form/function.checkbox.php
@@ -12,7 +12,7 @@
  */
 function smarty_function_checkbox($params, $smarty) 
 {
-    if(!isset($params['value'])) $params['value'] = 1;
+	if(!isset($params['value'])) $params['value'] = 1;
     
     $formParams = $smarty->_tag_stack[0][1];
 	$formHandler = $formParams['handle'];
