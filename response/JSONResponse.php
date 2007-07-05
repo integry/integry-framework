68f6e5aa8e19d5f7d278cbc035b6b58fe0e663e0
sergej <devnull@localhost>
2007-07-05 21:09:17 +0300

    [svn]

--- response/JSONResponse.php
+++ response/JSONResponse.php
@@ -11,12 +11,22 @@ class JSONResponse extends Response
 {
     private $content = "";
 
-	public function __construct($data)
+	public function __construct($data, $status = false, $message = false)
 	{
 	    $this->setHeader('Cache-Control', 'no-cache, must-revalidate');
 	    $this->setHeader('Expires', 'Mon, 26 Jul 1997 05:00:00 GMT');
 	    $this->setHeader('Content-type', 'text/javascript');
-        	    
+        
+	    if($message)
+	    {
+	        $data['message'] = $message;
+	    }
+	    
+	    if($status)
+	    {
+	        $data['status'] = strtolower($status);
+	    }
+	    
         $this->content = json_encode($data);
 	}
 
