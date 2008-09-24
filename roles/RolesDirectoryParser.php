fd1bcf86cab2b32abd725e2618b48e5bfb2364e4
Rinalds Uzkalns <rinalds@integry.net>
2008-09-24 15:44:56 +0300

    Roles parser did not work correctly if there were other files in controller directories

--- roles/RolesDirectoryParser.php
+++ roles/RolesDirectoryParser.php
@@ -3,7 +3,7 @@ ClassLoader::import("framework.roles.RolesParser");
 
 /**
  * @package	framework.roles
- * @author	Integry Systems 
+ * @author	Integry Systems
  */
 class RolesDirectoryParser
 {
@@ -11,13 +11,13 @@ class RolesDirectoryParser
 	private $directoryPath;
 	private $cacheDirectoryPath;
 	private $classParsers = array();
-	
-	public function __construct($directoryPath, $cacheDirectoryPath, $fileFilters = array('/\w+Controller\.php/'))
+
+	public function __construct($directoryPath, $cacheDirectoryPath, $fileFilters = array('/\w+Controller\.php$/'))
 	{
 		$this->fileFilters = $fileFilters;
 		$this->directoryPath = $directoryPath;
 		$this->cacheDirectoryPath = $cacheDirectoryPath;
-		
+
 		if(!is_dir($this->directoryPath))
 		{
 			throw new ApplicationException("Invalid directory path: {$this->directoryPath}");
@@ -26,18 +26,18 @@ class RolesDirectoryParser
 		{
 			throw new ApplicationException("Could not write to cache directory {$this->cacheDirectoryPath}");
 		}
-		else 
+		else
 		{
 			$this->parseDirectory();
 			$this->cache();
 		}
 	}
-	
+
 	public function getClassParsers()
 	{
 		return $this->classParsers;
 	}
-	
+
 	public function cache()
 	{
 		foreach($this->getClassParsers() as $classParser)
@@ -48,29 +48,29 @@ class RolesDirectoryParser
 			}
 		}
 	}
-	
+
 	private function parseDirectory()
 	{
 		if ($dir = opendir($this->directoryPath)) {
 			while (($controllerFile = readdir($dir)) !== false) {
 
 				if(!$this->matchFilters($controllerFile)) continue;
-					
+
 				$controllerName = substr($controllerFile, 0, -4);
 				$this->classParsers[] = new RolesParser(
-					$this->directoryPath . DIRECTORY_SEPARATOR . $controllerFile, 
+					$this->directoryPath . DIRECTORY_SEPARATOR . $controllerFile,
 					$this->cacheDirectoryPath . DIRECTORY_SEPARATOR . $controllerName . 'Roles.php'
 				);
 			}
-			
+
 			closedir($dir);
-		} 
+		}
 		else
 		{
 			throw new ApplicationException("Could not read from directory: {$this->directoryPath}");
 		}
 	}
-	
+
 	private function matchFilters($file)
 	{
 		$matchedFilters = true;
@@ -82,7 +82,7 @@ class RolesDirectoryParser
 				break;
 			}
 		}
-		
+
 		return $matchedFilters;
 	}
 }
