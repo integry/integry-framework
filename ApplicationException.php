7eb433f67e0d50f73a7747062c847d7c4b7127a8
Rinalds Uzkalns <rinalds@integry.net>
2009-01-16 07:55:59 +0200

    Minor fix

--- ApplicationException.php
+++ ApplicationException.php
@@ -6,84 +6,87 @@
  * @package framework
  * @author Integry Systems
  */
-class ApplicationException extends Exception 
+class ApplicationException extends Exception
 {
-			
+
 	public static function getFileTrace($trace)
 	{
 		$showedFiles = array();
 		$i = 0;
 		$traceString = '';
-				
+
 		$ajax = false; //isset($_SERVER['HTTP_X_REQUESTED_WITH']) ? true : false;
-		
-				
+
+
 		// Get new line
 		$newLine = $ajax ? "\n" : "<br />\n";
-				
+
 		foreach($trace as $call)
 		{
 			if(isset($call['file']) && isset($call['line']) && !isset($showedFiles[$call['file']][$call['line']]))
 			{
 				$showedFiles[$call['file']][$call['line']] = true;
-				
+
 				// Get file name and line
-				if($ajax) 
+				if($ajax)
 				{
 					$position = ($i++).": {$call['file']}:{$call['line']}";
 				}
-				else 
+				else
 				{
 					$position = "<strong>".($i++)."</strong>: \"{$call['file']}\":{$call['line']}";
 				}
-				
+
 				// Get function name
 				if(isset($call['class']) && isset($call['type']) && isset($call['function']))
 				{
 					$functionName = "{$call['class']}{$call['type']}{$call['function']}";
-				} 
-				else 
+				}
+				else
 				{
 					$functionName = $call['function'];
 				}
-				
+
 				// Get function arguments
 				$arguments = '';
 				$j = 1;
-				foreach($call['args'] as $argv) 
+				if (isset($call['args']))
 				{
-					switch(gettype($argv)) 
+					foreach($call['args'] as $argv)
 					{
-						case 'string':
-							$arguments .= "\"$argv\"";
-						break;
-						case 'boolean':
-							 $arguments .= ($argv ? 'true' : 'false');
-						break;
-						case 'integer':
-						case 'double':
-							 $arguments .= $argv;
-						break;
-						case 'object':
-							 $arguments .= "(object)" . get_class($argv);
-						break;
-						case 'array':
-							 $arguments .= "Array";
-						break;
-						default:
-							$arguments .= $argv;
-						break;
+						switch(gettype($argv))
+						{
+							case 'string':
+								$arguments .= "\"$argv\"";
+							break;
+							case 'boolean':
+								 $arguments .= ($argv ? 'true' : 'false');
+							break;
+							case 'integer':
+							case 'double':
+								 $arguments .= $argv;
+							break;
+							case 'object':
+								 $arguments .= "(object)" . get_class($argv);
+							break;
+							case 'array':
+								 $arguments .= "Array";
+							break;
+							default:
+								$arguments .= $argv;
+							break;
+						}
+
+						if($j < count($call['args'])) $arguments .= ", "; $j++;
 					}
-					
-					if($j < count($call['args'])) $arguments .= ", "; $j++;
 				}
 
-				
+
 				// format the output line
 				$traceString .= "$newLine$position - $functionName($arguments)";
 			}
 		}
-		
+
 		return $traceString;
 	}
 }
