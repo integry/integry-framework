babaac121174af26385a8bf49d1a9c1b05e5658b
Rinalds Uzkalns <rinalds@integry.net>
2011-06-08 20:48:11 +0300

    IsNotEmptyCheck works with arrays as well

--- request/validator/check/IsNotEmptyCheck.php
+++ request/validator/check/IsNotEmptyCheck.php
@@ -3,7 +3,7 @@
 ClassLoader::import("framework.request.validator.check.Check");
 
 /**
- * 
+ *
  *
  * @package framework.request.validator.check
  * @author Integry Systems
@@ -12,8 +12,13 @@ class IsNotEmptyCheck extends Check
 {
 	public function isValid($value)
 	{
+		if (is_array($value))
+		{
+			return !empty($value);
+		}
+
 		$value = trim($value);
-		
+
 		return (strlen($value) > 0);
 	}
 }
