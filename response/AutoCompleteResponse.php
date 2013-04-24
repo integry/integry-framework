c5d4e25b522781c4cc0693cffc0dd6b19a69a844
Rinalds Uzkalns <rinalds@integry.net>
2013-04-24 13:46:32 +0300

    AutoCompleteResponse returns JSON data instead of HTML

--- response/AutoCompleteResponse.php
+++ response/AutoCompleteResponse.php
@@ -8,36 +8,8 @@ ClassLoader::import("framework.response.Response");
  * @package	framework.response
  * @author Integry Systems
  */
-class AutoCompleteResponse extends Response
+class AutoCompleteResponse extends JSONResponse
 {
-	private $content = "";
-
-	public function __construct($data)
-	{
-		if (!is_array($data))
-		{
-		  	throw new Exception('AutoCompleteResponse::__construct needs an array passed!');
-		}
-				
-		$this->content = $data;
-	}
-
-	public function getData()
-	{
-		$listHtml = array('<ul>');
-	   	
-	  	$li = new HtmlElement('li');
-		foreach ($this->content as $key => $value)
-		{
-			$li->setAttribute('id', $key);
-			$li->setContent($value);
-			$listHtml[] = $li->render();
-		} 
-	   
-		$listHtml[] = '</ul>';
-
-		return implode("\n", $listHtml);
-	}
 }
 
 ?>
